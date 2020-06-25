03ae5f152ead3478e83035b8b840f04ed8ad8a64
Olivier Wysocinski <olivier.wys@gmail.Com>
2020-06-25 10:52:37 +0200

    Add the backend core

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function echoJson(array $content)
+{
+    echo json_encode($content);
+}
+
+function echoError(string $message)
+{
+    echoJson(['etat' => 'error', 'message' => $message]);
+}
+
+function echoSuccess(string $message, array $moreData = [])
+{
+    echoJson(array_merge(['etat' => 'success', 'message' => $message], $moreData));
+}
